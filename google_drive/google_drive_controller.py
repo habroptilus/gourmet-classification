cc574098216defeb3968dfedd71b2c0af8ec0fa8
habroptilus <x7deed53bc9k8@softbank.ne.jp>
2019-05-29 19:19:54 +0900

    pydriveつかわない

--- google_drive/google_drive_controller.py
+++ google_drive/google_drive_controller.py
@@ -9,9 +9,11 @@ class GoogleDriveController:
         gauth.LocalWebserverAuth()
         self.drive = GoogleDrive(gauth)
 
-    def fetch_fileinfo_list(self, dir):
+    def fetch_fileinfo_list(self, folder_id):
         """dir以下のファイル一覧を取得する."""
-        pass
+        query = f"'{folder_id}' in parents and trashed=false"
+        file_list = self.drive.ListFile({'q': query}).GetList()
+        return file_list
 
     def delete_files(self, file_list):
         """指定したfileのidリストに該当するファイルを一括削除する."""
@@ -19,9 +21,11 @@ class GoogleDriveController:
             x = self.drive.CreateFile({'id': i})
             x.Delete()
 
-    def download_files(self, id_list):
-        """idリストに該当するfileをダウンロードする."""
-        pass
+    def download_files(self, file_list, dir_name):
+        """idリストに該当するfileをdir_name以下にダウンロードする."""
+        for file in file_list:
+            x = self.drive.CreateFile({'id': file['id']})
+            x.GetContentFile(f"{dir_name}/{file['title']}")
 
     def upload_files(self, filepath_list, folder_id):
         """ローカルにあるファイルをアップロードする."""
@@ -33,6 +37,14 @@ class GoogleDriveController:
 
 
 if __name__ == "__main__":
-    from pathlib import Path
+    #from pathlib import Path
+    test_folder_id = "1sXEc0k3KwYpU5_rdyb9dq60De4uLzqQJ"
+    root_folder_id = "root"
+    gourmet_folder_id = "1bjbFGHOKPt_krVyewjoeLwRYLTKuwkMK"
+    model_folder_id = "1fbDIiefwMon8wF7VlOFUY15oTIufF6_Y"
+    photo_2019 = "19pyC-1HplZa0Aby2DtMarozjhA"
+
     controller = GoogleDriveController()
-    controller.upload_files([Path("./kakapo.jpg")], "root")
+    file_list = controller.fetch_fileinfo_list(photo_2019)
+    #controller.download_files(file_list, dir_name="images")
+    print(len(file_list))
